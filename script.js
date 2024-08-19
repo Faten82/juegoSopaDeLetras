aa278be37f6254dbf3a3f6d2b64049b487fcf902
LiliaPo <lidia.pototchi@xarxatecactiva.com>
2024-08-19 14:58:51 +0200

    inicioScript.js

---
+++ script.js
@@ -0,0 +1,14 @@
+//empiezo con array de palabras
+//escogi palabras relacionadas con programacion
+const todasLasPalabras = [
+    "JAVASCRIPT", "VARIABLE", "FUNCION", "ARRAY", "OBJETO", "LOOP", "CODIGO", "CLASE", 
+    "HTML", "CSS", "API", "DEBUG", "BROWSER", "SERVER", "CLIENTE", "ALGORITMO", "FRAMEWORK", 
+    "REPOSITORIO", "GIT", "JAVA", "PYTHON", "NODE", "REACT", "ANGULAR", "SQL", "DATABASE", 
+    "LOOPING", "SCRIPT", "FUNCTION", "CLASS", "METHOD", "PARAMETER", "RETURN", "EXCEPTION", 
+    "CONSOLE", "QUERY", "AJAX", "ASYNCHRONOUS", "PROMISE", "EVENT", "DEBUGGER", "VARIABLES", 
+    "OBJECTS", "PROTOTYPE", "MODULE", "PACKAGE", "COMPILER", "INTERPRETER", "CONSTRUCTOR"
+];
+const gridSize = 14;
+let grid = [];
+let palabras = [];
+let seleccion = [];
